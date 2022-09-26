90af2d2f23bc04076cb3cbdaba6d2494eba65127
Maxim <maxgerasin32@gmail.com>
2022-09-26 13:48:16 +0300

    DZ2

---
+++ DZ2/2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+Console.Clear();
+Console.Write("Enter numbers:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+string numInText = Convert.ToString(num);
+
+if (numInText.Length > 2)
+{
+  Console.WriteLine("Third nubmer:" + numInText[2]);
+}
+else {
+  Console.WriteLine("NO THIRD DIGIT !");
+} 
